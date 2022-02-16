4a62ef336618853ccb21ba1b46c888632beb1070
Sebastian Eudave <sebastian_eudave@epam.com>
2022-02-16 13:44:20 -0600

    Testing github hooks 2

--- src/test/java/test/Tests.java
+++ src/test/java/test/Tests.java
@@ -7,6 +7,7 @@ import static junit.framework.TestCase.assertEquals;
 public class Tests {
     @Test
     public void MavenParamTest(){
+        int a = 0;
         String browser = System.getProperty("browserName", "Chrome");
 
         assertEquals("Chrome", browser);
